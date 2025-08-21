18b6222746ba61c9ccdcd611b95fdc46b244d727
akitorahayashi <hayashi20030703@gmail.com>
2025-08-21 17:01:14 +0900

    apps/gist/apps.pyのnameを修正

--- apps/gist/apps.py
+++ apps/gist/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class GistConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "gist"
+    name = "apps.gist"
     label = "gist"
